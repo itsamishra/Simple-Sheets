cc011a33824b122c4665b7fb5cc371e5ec705bc4
Ash <ashutoshmishra413@gmail.com>
2019-04-17 19:28:07 -0700

    Started implementing cell extraction algorithm

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 
 #define CSV_FILE_NAME	"MOCK_DATA.csv"
 #define NUM_COLUMNS	5
 #define NUM_DATA_ROWS	100
+
+
+class Spreadsheet {
+	public:
+		std::string headerArray[NUM_COLUMNS];
+		std::string dataArray[NUM_DATA_ROWS][NUM_COLUMNS];
+};
+
 
 int main(int argc, char *argv[]){
 	// STEP 1: Load csv data from disk to memory
@@ -23,16 +30,51 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	// Reads csv file, line by line
+	// Feeds csv file into string line by line
 	std::string line;
-	std::string allCsvData = "";
+	std::string csvString = "";
 	while(getline(csv_file, line)){
-		allCsvData += line + ",";
+		csvString += line + ",";
 	}
-	std::cout << allCsvData << "\n";
+	//std::cout << csvString << "\n";
 
 	// Closes file
 	csv_file.close();
+
+	
+	// Extracts header and data from string	and loads it into Spreadsheet object
+	Spreadsheet spreadsheet;
+	int cellCount = 0;		// Keeps track of # of spreadsheet cells which have been parsed
+	int cellSize = 0;		// Keep track of string in cell's size in for loop below
+	bool headerParsed = false;	// Keeps track of whether the header has been parsed yet
+	for (int i=0; i<csvString.length(); i++){
+		// If comma is encountered, then current cell must have ended
+		if (csvString[i]==','){
+			// Extracts current cell
+			std::string currentCell = csvString.substr(i-cellSize, cellSize);
+			std::cout << currentCell << ",";
+
+			cellCount += 1;
+			if (cellCount % 5 == 0){
+				std::cout << "\n";
+			}
+			
+			// If header hasn't yet been parsed, parses header
+			if (headerParsed == false){
+				int arrayIndex = (cellCount%5)-1;
+				spreadsheet.headerArray[arrayIndex] = currentCell;
+				headerParsed = true;
+			}
+
+
+			// Resets cell size
+			cellSize = 0;
+		}
+		else {
+			cellSize += 1;
+		}
+	}
+	std::cout << cellCount << "\n";
 
 	// STEP 2: Save csv data from memory to disk
 	// - figure out how to write to file
