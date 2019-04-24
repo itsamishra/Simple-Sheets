d4754382f0ea32dfa8c8b6db6455c17c17358597
am <ashutoshmishra413@gmail.com>
2019-04-24 00:11:37 -0700

    Added getDataArray() function to Spreadsheet object

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <string>
 
-#define CSV_FILE_NAME "MOCK_DATA.csv"
-#define NUM_COLUMNS 5
-#define NUM_DATA_ROWS 100
+#define CSV_FILE_NAME   "MOCK_DATA.csv"
+#define NUM_COLUMNS     5
+#define NUM_DATA_ROWS   100
 
 class Spreadsheet {
 private:
@@ -34,59 +34,56 @@ private:
         int headerLength = 0;    // Keeps track of header length
         int numHeadersFound = 0; // Keeps track of # of headers found
         int numHeaders = NUM_COLUMNS; // Keeps track of # of headers that exist
-        for (uint i = 0; i < csvFileString.length(); i++) {
-            // If the next character is a comma, then we must be at the end of a
-            // header
-            if (csvFileString[i] == ',') {
+        for (uint i = 0; i < this->csvFileString.length(); i++) {
+            // If the next character is a comma, then we must be at the end of a header
+            if (this->csvFileString[i] == ',') {
                 // Extracts header from string
-                std::string header =
-                        this->csvFileString.substr(i - headerLength, headerLength);
+                std::string header = this->csvFileString.substr(i - headerLength, headerLength);
 
-                // Resets header length
+                // Resets header length to zero
                 headerLength = 0;
 
-                // Adds new header to Spreadsheet object and checks to see if there are
-                // any more headers to find
+                // Adds new header to Spreadsheet object and checks to see if there are any more headers to find
                 numHeadersFound++;
                 int headerIndex = numHeadersFound - 1;
                 this->headerArray[headerIndex] = header;
-                // If all headers have been found, records index at which headers end
-                // and exits loop
+
+                // If all headers have been found, records index at which headers end and exits loop
                 if (numHeadersFound == numHeaders) {
-                    this->dataStartIndex =
-                            i + 1; // Adds 1 so that index points to first character of data
+                    this->dataStartIndex = i + 1; // Adds 1 so that index points to first character of data
                     break;
                 }
-            } else {
+            }
+            // Else if the next character isn't a comma, increments length (i.e. size) of current header
+            else {
                 headerLength += 1;
             }
         }
     }
     // Sets value of dataArray[][]
-    // void setDataArray() {
-    //   // Loads data into Spreadsheet object
-    //   int dataLength = 0;   // Keeps track of the size of the data string
-    //   int numDataFound = 0; // Keeps track of # of data found
+     void setDataArray() {
+       // Loads data into Spreadsheet object
+       int dataLength = 0;   // Keeps track of the size of the data string
+       int numDataFound = 0; // Keeps track of # of data found
 
-    //   for (uint i = this->dataStartIndex; i < csvString.length(); i++) {
-    //     // std::cout << csvString[i];
+       for (uint i = this->dataStartIndex; i < this->csvFileString.length(); i++) {
 
-    //     if (csvString[i] == ',') {
-    //       std::string dataString = csvString.substr(i - dataLength, dataLength);
-    //       std::cout << dataString << "\n";
+         if (this->csvFileString[i] == ',') {
+           std::string dataString = this->csvFileString.substr(i - dataLength, dataLength);
+           //std::cout << dataString << "\n";
 
-    //       dataLength = 0;
+           dataLength = 0;
 
-    //       numDataFound++;
-    //       int dataIndex1 = (numDataFound - 1) / NUM_COLUMNS;
-    //       int dataIndex2 = (numDataFound - 1) % NUM_COLUMNS;
-    //       // std::cout << "-->(" << dataIndex1 << ", " << dataIndex2 << ")\n";
-    //       sheet.dataArray[dataIndex1][dataIndex2] = dataString;
-    //     } else {
-    //       dataLength += 1;
-    //     }
-    //   }
-    // }
+           numDataFound++;
+           int dataIndex1 = (numDataFound - 1) / NUM_COLUMNS;
+           int dataIndex2 = (numDataFound - 1) % NUM_COLUMNS;
+
+           this->dataArray[dataIndex1][dataIndex2] = dataString;
+         } else {
+           dataLength += 1;
+         }
+       }
+     }
 
 public:
     Spreadsheet(std::string csvFileName) {
@@ -94,7 +91,7 @@ public:
         setCsvFileString(csvFileName);
         // TODO: Handle possible error from getCsvString
         setHeaderArray();
-        // getDataArray();
+        setDataArray();
     }
 
     // Object attributes
@@ -110,83 +107,6 @@ int main(int argc, char *argv[]) {
     // Creates Spreadsheet object, which loads specified csv file into memory
     Spreadsheet spreadsheet(CSV_FILE_NAME);
     std::cout << "DONE\n";
-    /*
-    // Initialize Spreadsheet object
-    Spreadsheet sheet;
-
-    // Opens file in read mode
-    std::ifstream csv_file;
-    csv_file.open(CSV_FILE_NAME, std::ios::in);
-
-    // Makes sure file is open
-    if (csv_file.is_open() == false) {
-      std::cout << "ERROR: Could not open file\n";
-      return 1;
-    }
-
-    // Feeds csv file into string line by line
-    std::string line;
-    std::string csvString = "";
-    while (getline(csv_file, line)) {
-      csvString += line + ",";
-    }
-    // std::cout << csvString << "\n";
-
-    // Closes file
-    csv_file.close();
-
-    // Reads header into Spreadsheet object
-    int dataStartIndex = 0;  // Keeps track of string index at which the header
-    int headerLength = 0;    // Keeps track of header length
-    int numHeadersFound = 0; // Keeps track of # of headers found
-    int numHeaders = NUM_COLUMNS; // Keeps track of # of headers that exist
-    for (uint i = 0; i < csvString.length(); i++) {
-      // If the next character is a comma, then we must be at the end of a header
-      if (csvString[i] == ',') {
-        // Extracts header from string
-        std::string header = csvString.substr(i - headerLength, headerLength);
-
-        // Resets header length
-        headerLength = 0;
-
-        // Adds new header to Spreadsheet object and checks to see if there are
-        // any more headers to find
-        numHeadersFound++;
-        int headerIndex = numHeadersFound - 1;
-        sheet.headerArray[headerIndex] = header;
-        // If all headers have been found, records index at which headers end and
-        // exits loop
-        if (numHeadersFound == numHeaders) {
-          dataStartIndex =
-              i + 1; // Adds 1 so that index points to first character of data
-          break;
-        }
-      } else {
-        headerLength += 1;
-      }
-    }
-
-    // Loads data into Spreadsheet object
-    int dataLength = 0;   // Keeps track of the size of the data string
-    int numDataFound = 0; // Keeps track of # of data found
-    for (uint i = dataStartIndex; i < csvString.length(); i++) {
-      // std::cout << csvString[i];
-
-      if (csvString[i] == ',') {
-        std::string dataString = csvString.substr(i - dataLength, dataLength);
-        std::cout << dataString << "\n";
-
-        dataLength = 0;
-
-        numDataFound++;
-        int dataIndex1 = (numDataFound - 1) / NUM_COLUMNS;
-        int dataIndex2 = (numDataFound - 1) % NUM_COLUMNS;
-        // std::cout << "-->(" << dataIndex1 << ", " << dataIndex2 << ")\n";
-        sheet.dataArray[dataIndex1][dataIndex2] = dataString;
-      } else {
-        dataLength += 1;
-      }
-    }
 
     // STEP 2: Save csv data from memory to disk
     // - figure out how to write to file
@@ -198,5 +118,4 @@ int main(int argc, char *argv[]) {
     // - implement "Save" button
 
     return 0;
-  */
 }
